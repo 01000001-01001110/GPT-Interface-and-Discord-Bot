7b0a24443f653db9cbc04d455425bc2d79495dca
Alan Newingham <48245017+01000001-01001110@users.noreply.github.com>
2024-01-28 01:24:31 -0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,48 @@
-const { REST, Routes } = require('discord.js');
-const { token } = require('./config.json'); // Only your bot token is needed here
+import { REST, Routes } from 'discord.js';
+import config from './config.json' assert { type: 'json' };
 
-const commands = [{
-  name: 'ask',
-  description: 'Ask a question to the assistant',
-  options: [{
-    name: 'question',
-    type: 3,
-    description: 'The question you want to ask',
-    required: true,
-  }],
-}];
+const token = config.token;
+
+const commands = [
+  {
+    name: 'ask',
+    description: 'Ask a question to the assistant',
+    options: [{
+      name: 'question',
+      type: 3,
+      description: 'The question you want to ask',
+      required: true,
+    }],
+  },
+  {
+    name: 'roll',
+    description: 'Roll dice',
+    options: [
+      {
+        name: 'dice',
+        type: 4, // Corresponds to INTEGER type
+        description: 'Number of dice to roll',
+        required: false, // Make this optional; default can be set in the bot code
+      },
+      {
+        name: 'sides',
+        type: 4, // Corresponds to INTEGER type
+        description: 'Number of sides on each die',
+        required: false, // Make this optional; default can be set in the bot code
+      }
+    ]
+  }
+];
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands globally.');
-
     await rest.put(
       Routes.applicationCommands("1197652049043660842"), // Replace with your actual application ID
       { body: commands },
     );
-
     console.log('Successfully reloaded application (/) commands globally.');
   } catch (error) {
     console.error(error);
